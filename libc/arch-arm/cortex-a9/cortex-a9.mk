8aa5ce50eb005a09dfb76750d50b427f97f4e55b
timduru <timduru@gmail.com>
2015-07-02 09:48:59 +0200

    bionic: libc correctly use optimized neon and non-neon versions

--- libc/arch-arm/cortex-a9/cortex-a9.mk
+++ libc/arch-arm/cortex-a9/cortex-a9.mk
@@ -1,21 +1,26 @@
-$(call libc-add-cpu-variant-src,STRCAT,arch-arm/cortex-a9/bionic/strcat.S)
-$(call libc-add-cpu-variant-src,STRCMP,arch-arm/cortex-a9/bionic/strcmp.S)
-$(call libc-add-cpu-variant-src,STPCPY,arch-arm/cortex-a9/bionic/stpcpy.S)
+
+libc_bionic_src_files_arm += \
+    arch-arm/cortex-a9/bionic/strcat.S \
+    arch-arm/cortex-a9/bionic/strcmp.S \
+    arch-arm/cortex-a9/bionic/strcpy.S \
+    bionic/memmove.c \
+
 
 ifeq ($(strip $(ARCH_ARM_HAVE_NEON)),true)
-$(call libc-add-cpu-variant-src,MEMCPY,arch-arm/cortex-a9/bionic/memcpy.S)
-$(call libc-add-cpu-variant-src,MEMSET,arch-arm/cortex-a9/bionic/memset.S)
-
-$(call libc-add-cpu-variant-src,STRCPY,arch-arm/cortex-a9/bionic/strcpy.S)
-$(call libc-add-cpu-variant-src,STRLEN,arch-arm/cortex-a9/bionic/strlen.S)
-
-$(call libc-add-cpu-variant-src,__STRCAT_CHK,arch-arm/cortex-a9/bionic/__strcat_chk.S)
-$(call libc-add-cpu-variant-src,__STRCPY_CHK,arch-arm/cortex-a9/bionic/__strcpy_chk.S)
+libc_bionic_src_files_arm += \
+    arch-arm/cortex-a9/bionic/memcpy.S \
+    arch-arm/cortex-a9/bionic/memset.S \
+    arch-arm/cortex-a9/bionic/strcpy.S \
+    arch-arm/cortex-a9/bionic/strlen.S \
+    arch-arm/cortex-a9/bionic/__strcat_chk.S \
+    arch-arm/cortex-a9/bionic/__strcpy_chk.S \
 else
-$(call libc-add-cpu-variant-src,STRCPY,arch-arm/cortex-a9/bionic-linaro/strcpy.c)
-$(call libc-add-cpu-variant-src,STRLEN,arch-arm/cortex-a9/bionic-linaro/strlen.S)
+libc_bionic_src_files_arm += \
+    arch-arm/cortex-a9/bionic-linaro/strcpy.c \
+    arch-arm/cortex-a9/bionic-linaro/strlen.S \
+#generic
+    arch-arm/generic/bionic/memcpy.S \
+    arch-arm/generic/bionic/memset.S \
+    bionic/__strcat_chk.cpp \
+    bionic/__strcpy_chk.cpp \
 endif
-
-include bionic/libc/arch-arm/generic/generic.mk
-
-#    bionic/memmove.c \
