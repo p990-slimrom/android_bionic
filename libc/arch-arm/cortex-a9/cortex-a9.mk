6e60657cb9d4579b58718e9fdf08476e2a871a78
timduru <timduru@gmail.com>
2015-07-02 10:16:06 +0200

    a9: fix duplicated strcpy

--- libc/arch-arm/cortex-a9/cortex-a9.mk
+++ libc/arch-arm/cortex-a9/cortex-a9.mk
@@ -12,12 +12,10 @@ ifeq ($(strip $(ARCH_ARM_HAVE_NEON)),true)
 libc_bionic_src_files_arm += \
     arch-arm/cortex-a9/bionic/memcpy.S \
     arch-arm/cortex-a9/bionic/memset.S \
-    arch-arm/cortex-a9/bionic/strcpy.S \
     arch-arm/cortex-a9/bionic/__strcat_chk.S \
     arch-arm/cortex-a9/bionic/__strcpy_chk.S \
 else
 libc_bionic_src_files_arm += \
-    arch-arm/cortex-a9/bionic-linaro/strcpy.c \
 #generic
     arch-arm/generic/bionic/memcpy.S \
     arch-arm/generic/bionic/memset.S \
