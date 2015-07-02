a64873da15d5b20249eca39e9861f5f408c4f2da
timduru <timduru@gmail.com>
2015-07-02 10:24:01 +0200

    fix

--- libc/arch-arm/cortex-a9/cortex-a9.mk
+++ libc/arch-arm/cortex-a9/cortex-a9.mk
@@ -5,20 +5,19 @@ libc_bionic_src_files_arm += \
     arch-arm/cortex-a9/bionic/strcpy.S \
     bionic/memmove.c \
     arch-arm/cortex-a15/bionic/strlen.S \
-    arch-arm/cortex-a15/bionic/memchr.S \
-
+    arch-arm/cortex-a15/bionic/memchr.S
 
 ifeq ($(strip $(ARCH_ARM_HAVE_NEON)),true)
 libc_bionic_src_files_arm += \
     arch-arm/cortex-a9/bionic/memcpy.S \
     arch-arm/cortex-a9/bionic/memset.S \
     arch-arm/cortex-a9/bionic/__strcat_chk.S \
-    arch-arm/cortex-a9/bionic/__strcpy_chk.S \
+    arch-arm/cortex-a9/bionic/__strcpy_chk.S
 else
-libc_bionic_src_files_arm += \
 #generic
+libc_bionic_src_files_arm += \
     arch-arm/generic/bionic/memcpy.S \
     arch-arm/generic/bionic/memset.S \
     bionic/__strcat_chk.cpp \
-    bionic/__strcpy_chk.cpp \
+    bionic/__strcpy_chk.cpp
 endif
