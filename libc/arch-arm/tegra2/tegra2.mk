e2261d64ca91fc15e87c8e1da11a3da13af47392
gucky01 <gutentach@unitybox.de>
2016-05-30 21:15:16 +0200

    tegra 2 bionic

---
+++ libc/arch-arm/tegra2/tegra2.mk
@@ -0,0 +1,12 @@
+libc_bionic_src_files_arm += \
+    arch-arm/tegra2/bionic/memchr.S \
+    arch-arm/tegra2/bionic/memcpy.S \
+    arch-arm/tegra2/bionic/memset.S \
+    upstream-openbsd/lib/libc/string/strcat.c \
+    arch-arm/tegra2/bionic/strcmp.S \
+    arch-arm/tegra2/bionic/strcpy.S \
+    arch-arm/tegra2/bionic/strlen.S \
+    bionic/__strcat_chk.cpp \
+    bionic/__strcpy_chk.cpp \
+    bionic/memmove.c
+    
